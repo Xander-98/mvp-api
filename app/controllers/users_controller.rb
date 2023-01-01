6c17db65af36da8dfc204a0807fabb3956269c1e
James Alexander <jaymealexander.12@gmail.com>
2023-01-01 14:39:19 -0600

    adds user create function and the ability to log in/out via sessions controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render template: "users/new"
+  end
+
+  def create
+    @user = User.new(
+      username: params[:user][:username],
+      email: params[:user][:email],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation],
+    )
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
