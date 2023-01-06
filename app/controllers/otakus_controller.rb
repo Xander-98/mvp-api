96adea485682e180d099ef28f0f0b222c5b485f8
James Alexander <jaymealexander.12@gmail.com>
2023-01-05 21:29:31 -0600

    adds get_anime function in otakus controller which replaces api call in frontend, will have to do the same process for anime search function from frontend

--- app/controllers/otakus_controller.rb
+++ app/controllers/otakus_controller.rb
@@ -1,9 +1,25 @@
 class OtakusController < ApplicationController
-  def create
+  def new
     otaku = Otaku.create(
       anime_id: params[:anime_id],
       user_id: params[:user_id],
     )
     render json: otaku.as_json
+  end
+
+  def all_anime
+    animes = HTTP.get("https://api.jikan.moe/v4/top/anime?filter=%22bypopularity%22&limit=10")
+    anime_data = animes.parse(:json)
+    render json: anime_data.as_json
+  end
+
+  def search_anime
+    if params[:search].blank?
+    else
+      query = params[:search]
+      anime_search = HTTP.get("https://api.jikan.moe/v4/anime?q=" + query + "&order_by=title&sort=asc")
+      anime_search_data = anime_search.parse(:json)
+      render json: anime_search_data.as_json
+    end
   end
 end
